041f3b7136a80861040ff7b04896d9a595044021
Venkatesh Dasari <venkateshdasari021@gmail.com>
2023-08-10 12:06:14 +0530

    .gitignore and .env are added

--- flights/views.py
+++ flights/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render
-from .models import Flight, Airport
+from .models import Flight, Airport,Passenger
 
 # Create your views here.
 
@@ -13,6 +13,29 @@ def index(request):
 
 def flight(request, flight_id):
     flight = Flight.objects.get(id=flight_id)
+    passengers = flight.passengers.all()
+    non_passengers = Passenger.objects.exclude(flights=flight).all()
     return render(request, "flights/flight.html", {
-        "flight": flight
+        "flight": flight,
+        "passengers" : passengers
     })
+
+def book(request, flight_id):
+
+    # For a post request, add a new flight
+    if request.method == "POST":
+
+        # Accessing the flight
+        flight = Flight.objects.get(pk=flight_id)
+
+        # Finding the passenger id from the submitted form data
+        passenger_id = int(request.POST["passenger"])
+
+        # Finding the passenger based on the id
+        passenger = Passenger.objects.get(pk=passenger_id)
+
+        # Add passenger to the flight
+        passenger.flights.add(flight)
+
+        # Redirect user to flight page
+        return HttpResponseRedirect(reverse("flight", args=(flight.id,)))
